b056c940d184a7530d95cd01a6c4be82b5750842
David Heinemeier Hansson <david@hey.com>
2025-08-23 23:03:54 +0200

    Switch Zoom from shitty native app to better webapp
    Have to compromise on 1080p but seems better than dealing with that
    trash app.

--- install/apps/webapps.sh
+++ install/apps/webapps.sh
@@ -6,4 +6,5 @@ if [ -z "$OMARCHY_BARE" ]; then
   omarchy-webapp-install "ChatGPT" https://chatgpt.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
   omarchy-webapp-install "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
   omarchy-webapp-install "Figma" https://figma.com/ https://www.veryicon.com/download/png/application/app-icon-7/figma-1?s=256
+  omarchy-webapp-install "Zoom" https://app.zoom.us/wc/home https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/zoom.png
 fi

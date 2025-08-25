935d536e37cbafb9cd4e6a17a2830e801cc15c0a
David Heinemeier Hansson <david@hey.com>
2025-08-25 07:04:43 +0200

    Rip out the last remnants of AUR

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -4,6 +4,6 @@ sudo pacman -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
   wl-clip-persist \
   nautilus sushi ffmpegthumbnailer gvfs-mtp \
-  slurp satty wf-recorder \
+  slurp satty wf-recorder wl-screenrec \
   mpv evince imv \
   walker-bin google-chrome
